d430c73f873bcddc3c61a718fea9f209c6187918
민수 <109027302+Pushedsu@users.noreply.github.com>
2023-10-14 23:30:01 +0900

    비밀번호 변경 dto 생성

---
+++ src/user/dto/user.req.updatepw.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { User } from '../user.schema';
+import { Prop } from '@nestjs/mongoose';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Types } from 'mongoose';
+
+export class UserReqUpdatePasswordDto extends PickType(User, [
+  'password',
+] as const) {
+  @ApiProperty({
+    example: '32d804we9t9...',
+    description: 'id',
+    required: true,
+  })
+  @Prop()
+  @IsString()
+  @IsNotEmpty()
+  id: Types.ObjectId;
+
+  @ApiProperty({
+    description: 'password',
+    required: true,
+  })
+  @Prop()
+  @IsString()
+  @IsNotEmpty()
+  currentPassword: string;
+}
