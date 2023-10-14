154feca30c12a8ae1573b0d48a74a5a2fa38838b
민수 <109027302+Pushedsu@users.noreply.github.com>
2023-10-14 23:29:37 +0900

    이름 변경 dto 생성

---
+++ src/user/dto/user.req.updateName.dto.ts
@@ -0,0 +1,17 @@
+import { Prop } from '@nestjs/mongoose';
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { User } from '../user.schema';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Types } from 'mongoose';
+
+export class UserReqUpdateNameDto extends PickType(User, ['name'] as const) {
+  @ApiProperty({
+    example: '32d804we9t9...',
+    description: 'id',
+    required: true,
+  })
+  @Prop()
+  @IsString()
+  @IsNotEmpty()
+  id: Types.ObjectId;
+}
