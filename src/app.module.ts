ddfa23e9cf499ca63424ed0af1cce8b6b0ffaaa8
민수 <109027302+Pushedsu@users.noreply.github.com>
2024-01-14 20:50:00 +0900

    이메일 기능 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PostModule } from './posts/posts.module';
 import { AwsModule } from './aws/aws.module';
+import { EmailModule } from './email/email.module';
 import mongoose from 'mongoose';
 
 @Module({
@@ -21,9 +20,8 @@ import mongoose from 'mongoose';
     AuthModule,
     PostModule,
     AwsModule,
+    EmailModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements NestModule {
   private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
