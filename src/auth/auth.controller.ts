c81777e5bdfd35f0aa99a251b8645b3bcc0225aa
민수 <109027302+Pushedsu@users.noreply.github.com>
2024-10-06 22:20:52 +0900

    swagger api 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,12 +10,15 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { GooglAuthGuard } from './google/google.guard';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { UserRepository } from 'src/user/user.repository';
 import { UserService } from 'src/user/service/user.service';
 import { HttpExceptionFilter } from 'src/common/exceptions/http-exceptions.filter';
 import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 
+@ApiTags('Auth Google')
+@UseFilters(HttpExceptionFilter)
 @Controller('auth')
 export class AuthController {
   constructor(
@@ -24,6 +27,11 @@ export class AuthController {
     private readonly userService: UserService,
   ) {}
 
+  @ApiOperation({ summary: '구글 로그인 토큰 검증' })
+  @ApiResponse({
+    status: 500,
+    description: 'Server error...',
+  })
   @Post('google/idToken')
   @UseFilters(HttpExceptionFilter)
   @UseInterceptors(SuccessInterceptor)
@@ -31,11 +39,21 @@ export class AuthController {
     return await this.authService.verifyToken(idToken);
   }
 
+  @ApiOperation({ summary: '구글 Auth 초기 생성' })
+  @ApiResponse({
+    status: 500,
+    description: 'Server error...',
+  })
   @Get('google')
   @UseGuards(GooglAuthGuard)
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   async googleLogin(@Req() req) {}
 
+  @ApiOperation({ summary: '구글 Auth 리디렉션 기능' })
+  @ApiResponse({
+    status: 500,
+    description: 'Server error...',
+  })
   @Get('redirect')
   @UseGuards(GooglAuthGuard)
   async googleLoginCallback(@Req() req, @Res() res) {
